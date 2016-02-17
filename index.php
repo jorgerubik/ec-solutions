8245cd556f5d8f89ed4e5e2ae0c497efca23a476
Jorge Hernandez <jorge_delux_123@hotmail.com>
2016-02-17 16:34:02 -0600

    interiores acabados

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $idioma = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
  
 if($idioma=='en'){
-    header("Location: index-en.html");
+    header("Location: index-es.html");
 }else{
     header("Location: index-es.html");
 }
